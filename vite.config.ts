6014d2812d072eefb2c7c6902fa36703708ba1c5
Matthew Vance <mavance44@gmail.com>
2024-12-04 17:19:33 -0500

    add tailwind

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,21 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import vueDevTools from "vite-plugin-vue-devtools";
-
+import autoprefixer from "autoprefixer";
+import tailwind from "tailwindcss";
 import electron from "vite-plugin-electron/simple";
 
 // https://vite.dev/config/
 export default defineConfig({
   build: {
     minify: false,
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
   },
   plugins: [
     vue(),
