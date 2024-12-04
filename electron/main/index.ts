c2d0b33cd7bc72a2be15e8448c4aee00cb4f373a
Matthew Vance <mavance44@gmail.com>
2024-12-03 21:39:11 -0500

    setup electron

---
+++ electron/main/index.ts
@@ -0,0 +1,33 @@
+import { app, BrowserWindow } from 'electron'
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    title: 'PDF Generator Prototype',
+    width: 1320,
+    height: 1064,
+    backgroundColor: '#181818',
+  })
+
+  win.once('ready-to-show', () => {
+    win.show()
+  })
+
+  if (process.env.VITE_DEV_SERVER_URL) {
+    win.loadURL(process.env.VITE_DEV_SERVER_URL)
+    win.webContents.openDevTools()
+  } else {
+    win.loadFile('dist/index.html')
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
--- vite.config.ts
+++ vite.config.ts
@@ -5,16 +5,27 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
+import electron from 'vite-plugin-electron/simple'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    electron({
+      main: {
+        entry: 'electron/main/index.ts',
+      },
+      preload: {
+        input: 'electron/preload/index.ts',
+      },
+      renderer: {},
+    }),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
