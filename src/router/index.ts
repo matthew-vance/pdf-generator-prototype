4541ecb8509ee3f13693330657e55c1122d4ae3d
Matthew Vance <mavance44@gmail.com>
2024-12-05 00:22:12 -0500

    always use hash router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,26 +1,16 @@
-import {
-  createRouter,
-  createWebHistory,
-  createWebHashHistory,
-} from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 const router = createRouter({
-  history: import.meta.env.VITE_DEV_SERVER_URL
-    ? createWebHistory(import.meta.env.BASE_URL)
-    : createWebHashHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: "/",
       name: "home",
-      component: HomeView,
+      component: () => import("../views/HomeView.vue"),
     },
     {
       path: "/about",
       name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
     },
   ],
