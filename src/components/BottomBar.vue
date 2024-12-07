38edfe46df958a7e39821cc9e7591dc8b4468169
Matthew Vance <mavance44@gmail.com>
2024-12-07 16:51:21 -0500

    re-add settings modal placeholder

--- src/components/BottomBar.vue
+++ src/components/BottomBar.vue
@@ -1,12 +1,40 @@
 <script setup lang="ts">
 import { Settings } from "lucide-vue-next";
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogClose,
+} from "@/components/ui/dialog";
 </script>
 
 <template>
   <div class="flex justify-between items-center h-full px-4">
-    <Button variant="ghost" size="icon">
-      <Settings />
-    </Button>
+    <Dialog>
+      <DialogTrigger as-child>
+        <Button variant="ghost" size="icon">
+          <Settings />
+        </Button>
+      </DialogTrigger>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Settings</DialogTitle>
+        </DialogHeader>
+        <DialogDescription>
+          <p>User setting will be here!</p>
+        </DialogDescription>
+        <DialogFooter>
+          <Button>Save</Button>
+          <DialogClose as-child>
+            <Button variant="secondary">Cancel</Button>
+          </DialogClose>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
   </div>
 </template>
